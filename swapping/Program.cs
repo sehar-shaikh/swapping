8948d7e601901e416357e63d9d85362a0a52ad9d
Sehar <Sehar@DESKTOP-F5C6FMG>
2023-03-28 20:27:29 +0500

    Add project files.

---
+++ swapping/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace swapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            Console.WriteLine("veribles after swapping ");
+           
+            a = a + b; //30
+            b = a - b; // 10
+            a = a - b;
+
+            Console.WriteLine("a =" + a);
+            Console.WriteLine("b =" + b);
+
+            Console.ReadLine();
+
+           
+
+
+        }
+    }
+}
